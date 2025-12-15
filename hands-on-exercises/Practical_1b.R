83764e88e70681b743a68285cbad1fbf15dba5cd
g_janova <50871205+GXM4@users.noreply.github.com>
2025-12-15 16:58:30 +0000

    Update header and comments for version 1.2.0
    Updated the header and comments to reflect version 1.2.0 and maintain consistency in the documentation.

--- hands-on-exercises/Practical_1b.R
+++ hands-on-exercises/Practical_1b.R
@@ -2,7 +2,7 @@
 #
 #   ###  G I D R O L O G I Y A D A   R   D A N   F O Y D A L A N I S H  ###
 #   ###             U S I N G   R   I N    H Y D R O L O G Y            ###
-#   ###                         Version 1.0                             ###  
+#   ###                         Version 1.2.0                             ###  
 #        
 #       Mualliflar / Authors: ZULFIYA Kuranboyeva & GAVKHAR Mamadjanova
 #                       
@@ -65,3 +65,4 @@ title("Suv sarfining yil ichida tebranish grafigi")
 # ------ 5. Grafikni png faylda saqlash uchun buyruqni yakunlash ---------------
 
 dev.off() 
+
