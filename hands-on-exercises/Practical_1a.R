7595c9e6a591940dcc03a6f10021f0031d5a05af
g_janova <50871205+GXM4@users.noreply.github.com>
2025-12-15 16:58:12 +0000

    Update header and comments in Practical_1a.R

--- hands-on-exercises/Practical_1a.R
+++ hands-on-exercises/Practical_1a.R
@@ -2,7 +2,7 @@
 #
 #   ###  G I D R O L O G I Y A D A   R   D A N   F O Y D A L A N I S H  ###
 #   ###             U S I N G   R   I N    H Y D R O L O G Y            ###
-#   ###                         Version 1.0                             ###  
+#   ###                         Version 1.2.0                             ###  
 #        
 #       Mualliflar / Authors: ZULFIYA Kuranboyeva & GAVKHAR Mamadjanova
 #                       
@@ -103,4 +103,5 @@ legend("topright",
 # ------ 5. Grafikni png faylda saqlash uchun buyruqni yakunlash ---------------
 
 dev.off()
+
 
