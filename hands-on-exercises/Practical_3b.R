68df83034f1eaac298213a815ca31b7569d87059
g_janova <50871205+GXM4@users.noreply.github.com>
2025-12-29 14:26:06 +0000

    Update data source and variable names in Practical 3b

--- hands-on-exercises/Practical_3b.R
+++ hands-on-exercises/Practical_3b.R
@@ -14,14 +14,14 @@
 # ---------------------------------------------------------------------------- #
 
 # Readme in Uzbek   R 4.5.0
-# 3b-amaliy mashg'ulotda Norin daryosi Kokjerdi va Koykerim gidropostlarida tanlab
+# 3b-amaliy mashg'ulotda Norin daryosi Kokjerdi va Kyokerim gidropostlaridan tanlab
 # olingan suv sathlari orasidagi to'g'ri chiziqli bog'lanishni aniqlash usuli ko'rsatilgan. 
 # Buyruqlarni yurguzish to'g'ri amalga oshirilganda olingan natijalar Figure_3b.png
 # dagi grafik ko'rinishida bo'ladi.
 
 # ----- 1. Ma'lumotlarni o'qish va tez qarab chiqish  --------------------------
 
-file3 <- read.csv("C:/Users/Data/monthly_peaks_and_lows.csv", header = TRUE)
+file3 <- read.csv("C:/Users/Data/monthly_peaks_and_lows_paired.csv", header = TRUE)
 summary(file3)
 
 # ------ 2. 3b-grafikni png faylda saqlash uchun joyni ko'rsatish --------------
@@ -31,31 +31,31 @@ png("C:/Users/Figures/Figure_3b.png",
 
 # ------ 3. To'g'ri chiziqli bog'lanish tenglamasi ------------------------------
 
-model <- lm(Koykerim ~ Kokjerdi, data = file3)
+model <- lm(Kyokerim_match_H ~ Kokjerdi, data = file3)
 summary(model)
 
 # ------ 4. Grafikni chizish va chiziqli bog'lanishni ko'rsatish ----------------
 
 plot(
-  file3$Kokjerdi, file3$Koykerim,
-  col = "red",
+  file3$Kokjerdi, file3$Kyokerim_match_H,
+  col = "cornflowerblue",
   pch = 16,
   cex = 1.5,
   xlim = c(200, 600),
   ylim = c(100, 550),
   xlab = "Kokjerdi, H (cm)",
-  ylab = "Koykerim, H (cm)",
+  ylab = "Kyokerim, H (cm)",
   main = "Moslashgan suv sathlarining bog‘lanish grafigi"
 )
 
 # ------ 5. Regressiya chizig'ini o'tkazish va R qiymatini ko'rsatish -----------
 
-abline(model, col = "blue", lwd = 3)
+abline(model, col = "violet", lwd = 3)
 
 legend(
   "bottomright",
   bty = "n", cex = 1.2,
-  legend = paste("Adjusted R² =", round(summary(model)$adj.r.squared, 3))
+  legend = paste("R² =", round(summary(model)$adj.r.squared, 3))
 )
 
 dev.off()
