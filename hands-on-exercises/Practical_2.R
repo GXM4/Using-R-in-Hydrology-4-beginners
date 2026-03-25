98bec6c9b7abd083f95c615b3f26ac50d45cd041
Gavkhar Mamadjanova <50871205+GXM4@users.noreply.github.com>
2026-03-25 10:16:20 +0000

    Restore header and comments in Practical_2.R

--- hands-on-exercises/Practical_2.R
+++ hands-on-exercises/Practical_2.R
@@ -1,9 +1,7 @@
 # ==============================================================================
-#
 #   ###  G I D R O L O G I Y A D A   R   D A N   F O Y D A L A N I S H  ###
 #   ###             U S I N G   R   I N    H Y D R O L O G Y            ###
-#   ###                         Version 1.2.0                           ###  
-#        
+#   ###                         Version 1.2.0                           ###          
 #       Mualliflar / Authors: ZULFIYA Kuranboyeva & GAVKHAR Mamadjanova
 # ==============================================================================
 #
