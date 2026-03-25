0063809cad3c57732a8ce8328db7e19c7ead18c4
Gavkhar Mamadjanova <50871205+GXM4@users.noreply.github.com>
2026-03-25 10:15:24 +0000

    Update comments and formatting in Practical_1b.R

--- hands-on-exercises/Practical_1b.R
+++ hands-on-exercises/Practical_1b.R
@@ -1,16 +1,13 @@
 # ==============================================================================
-#
 #   ###  G I D R O L O G I Y A D A   R   D A N   F O Y D A L A N I S H  ###
 #   ###             U S I N G   R   I N    H Y D R O L O G Y            ###
 #   ###                         Version 1.2.0                           ###  
-#        
-#       Mualliflar / Authors: ZULFIYA Kuranboyeva & GAVKHAR Mamadjanova
-#                       
+#       Mualliflar / Authors: ZULFIYA Kuranboyeva & GAVKHAR Mamadjanova                      
 # ==============================================================================
 #
 # ---------------------------------------------------------------------------- #
 #                       1b-amaliy mashg'ulot / Practical 1b                    #
-#  Suv sathining kompleks garfigini chizish / Composite graph of water level   #
+#  Suv sathining kompleks garfigini chizish / Composite graph of water levels  #
 # ---------------------------------------------------------------------------- #
 
 # Code description in Uzbek     R 4.5.0
