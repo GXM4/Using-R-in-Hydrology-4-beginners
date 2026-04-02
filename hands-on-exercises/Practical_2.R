593e06405901273b913b348e65a0586720b36cca
Gavkhar Mamadjanova <50871205+GXM4@users.noreply.github.com>
2026-04-02 18:12:00 +0100

    Update Practical_2.R

--- hands-on-exercises/Practical_2.R
+++ hands-on-exercises/Practical_2.R
@@ -1,9 +1,4 @@
-# ==============================================================================
-#   ###  G I D R O L O G I Y A D A   R   D A N   F O Y D A L A N I S H  ###
-#   ###             U S I N G   R   I N    H Y D R O L O G Y            ###
-#   ###                         Version 1.2.0                           ###          
-#       Mualliflar / Authors: ZULFIYA Kuranboyeva & GAVKHAR Mamadjanova
-# ==============================================================================
+# Code authors: ZULFIYA Kuranboyeva & GAVKHAR Mamadjanova   
 #
 # ---------------------------------------------------------------------------- #
 #                       2-amaliy mashg'ulot / Practical 2                      #
