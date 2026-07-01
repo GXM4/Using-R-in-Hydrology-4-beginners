5d835125ac53dcaadc16f26248301f3cb74d93d9
Zulfiya <96618102+marsova99@users.noreply.github.com>
2026-07-01 10:32:27 +0500

    Update Practical_3b.R

--- hands-on-exercises/Practical_3b.R
+++ hands-on-exercises/Practical_3b.R
@@ -1,12 +1,18 @@
-#     Mualliflar / Code authors: ZULFIYA Kuranboyeva & GAVKHAR Mamadjanova     #
-#                                                                              #
+# ==============================================================================
+#     G I D R O L O G I Y A D A   R   D A N   F O Y D A L A N I S H  
+#                U S I N G   R   I N    H Y D R O L O G Y            
+#                           Version 1.2.0                             
+#        
+#       Mualliflar / Authors: ZULFIYA Kuranboyeva & GAVKHAR Mamadjanova
+# ==============================================================================
+#
 # ---------------------------------------------------------------------------- #
 #                       3b-amaliy mashg'ulot / Practical 3b                    #
 #               Moslashgan suv sathlari orasidagi bog'lanishni aniqlash        #  
 #                 Linear relationship between adjusted water level             #
 # ---------------------------------------------------------------------------- #
-#
-# Code description in Uzbek |  R version >4.5.0
+
+# Readme in Uzbek   R 4.5.0
 # 3b-amaliy mashg'ulotda Norin daryosi Kokjerdi va Kyokerim gidropostlaridan tanlab
 # olingan suv sathlari orasidagi to'g'ri chiziqli bog'lanishni aniqlash usuli ko'rsatilgan. 
 # Buyruqlarni yurguzish to'g'ri amalga oshirilganda olingan natijalar Figure_3b.png
@@ -14,41 +20,52 @@
 
 # ----- 1. Ma'lumotlarni o'qish va tez qarab chiqish  --------------------------
 
-file3 <- read.csv("C:/Users/Data/monthly_peaks_and_lows_paired.csv", header = TRUE)
+file3 <- read.csv("C:/User/Data/UGAM_river.csv",
+#ushbu qator foydalanuvchi tomonidan tahrirlanadi 
+        header = TRUE
+)
 summary(file3)
 
+x<-file3$Kokjerdi
+y<-file3$Kyokerim_match_H
 # ------ 2. 3b-grafikni png faylda saqlash uchun joyni ko'rsatish --------------
 
 png("C:/Users/Figures/Figure_3b.png",
-    width = 6, height = 6, units = "in", res = 300)
+    width = 6, height = 6, units = "in", res = 300) 
+#ushbu qator foydalanuvchi tomonidan tahrirlanadi 
 
-# ------ 3. To'g'ri chiziqli bog'lanish tenglamasi ------------------------------
+# ------ 3. To'g'ri chiziqli bog'lanish tenglamasi -----------------------------
 
 model <- lm(Kyokerim_match_H ~ Kokjerdi, data = file3)
 summary(model)
 
-# ------ 4. Grafikni chizish va chiziqli bog'lanishni ko'rsatish ----------------
+# ------ 4. Grafikni chizish va chiziqli bog'lanishni ko'rsatish ---------------
 
-plot(
-  file3$Kokjerdi, file3$Kyokerim_match_H,
-  col = "cornflowerblue",
-  pch = 16,
-  cex = 1.5,
-  xlim = c(200, 600),
-  ylim = c(100, 550),
-  xlab = "Kokjerdi, H (cm)",
-  ylab = "Kyokerim, H (cm)",
-  main = "Moslashgan suv sathlarining bog‘lanish grafigi"
-)
+plot(x, y, pch = 16, col = "cornflowerblue",
+     cex = 1.5,
+     xlim = c(340, 510),
+     ylim = c(150, 400),
+     xlab = "Kokjerdi, H (cm)",
+     ylab = "Kyokerim, H (cm)",
+     main = "Moslashgan suv sathlarining bog‘lanish grafigi")
 
-# ------ 5. Regressiya chizig'ini o'tkazish va R qiymatini ko'rsatish -----------
 
-abline(model, col = "violet", lwd = 3)
+abline(model, col = 4, lwd = 3)# Regressiya chizig'ini o'tkazish
+
+segments(x0 = x, x1 = x, y0 = y, y1 = predict(model),
+         lwd = 1, col = "red") #segmentlar va tenglamalar bo'yicha buyruqni berish
+
+coef <- round(coef(model), 2) #koeffitsiyentni yaxlitlash
+
+# ------ 5. R qiymati va umumiy tenglamani ko'rsatish --------------------------
 
 legend(
   "bottomright",
-  bty = "n", cex = 1.2,
-  legend = paste("R² =", round(summary(model)$adj.r.squared, 3))
+  bty = "n", cex = 1.1,
+  legend = c(
+    paste("y =", model_coef[1], ifelse(model_coef[2] >= 0, "+", "-"), abs(model_coef[2]), "x"),
+    paste("R² =", round(summary(model)$adj.r.squared, 3))
+  )
 )
 
 dev.off()
